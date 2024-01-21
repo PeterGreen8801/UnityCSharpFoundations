155b3c9242a82ec9577871ab6cbf1fbe9a0118f0
Peter <petergreen880@yahoo.com>
2024-01-20 23:05:58 -0500

    Did higher or lower challenge

--- MonsterMathIsland/Assets/QuestionManager.cs
+++ MonsterMathIsland/Assets/QuestionManager.cs
@@ -6,6 +6,10 @@ using TMPro;
 public class QuestionManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _messageBoxTextField;
+    [SerializeField] private TMP_InputField _answerInputField;
+
+    string question;
+    [SerializeField] int answer;
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +18,43 @@ public class QuestionManager : MonoBehaviour
         int operand1 = Random.Range(1, 100);
         int operand2 = Random.Range(1, 100);
 
-        Debug.Log(operand1);
-        Debug.Log(operand2);
+        if (Random.value < 0.5f)
+        {
+            Debug.Log("This is addition");
+            question = $"{operand1} + {operand2} =";
 
-        _messageBoxTextField.text = $"{operand1} + {operand2} =";
+            answer = operand1 + operand2;
+        }
+        else
+        {
+            Debug.Log("This is subtraction");
+            question = $"{operand1} - {operand2} =";
 
+            answer = operand1 - operand2;
+        }
+
+        _messageBoxTextField.text = question;
+
+        _answerInputField.Select();
+
+
+        int rando1 = Random.Range(1, 100);
+        Debug.Log(rando1);
+        int rando2 = Random.Range(1, 100);
+        Debug.Log(rando2);
+
+        if (rando1 < rando2)
+        {
+            Debug.Log(rando1 + " is less than " + rando2);
+        }
+        else if (rando2 == rando1)
+        {
+            Debug.Log(rando1 + " is the same as " + rando2);
+        }
+        else
+        {
+            Debug.Log(rando1 + " is more than " + rando2);
+        }
     }
 
     // Update is called once per frame
