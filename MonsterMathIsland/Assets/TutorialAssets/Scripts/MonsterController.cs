c03cfe2e59c91effa29dd172164217e4252a769b
Peter <petergreen880@yahoo.com>
2024-01-22 19:00:18 -0500

    Transferred new files

--- MonsterMathIsland/Assets/TutorialAssets/Scripts/MonsterController.cs
+++ MonsterMathIsland/Assets/TutorialAssets/Scripts/MonsterController.cs
@@ -12,22 +12,31 @@ namespace TutorialAssets.Scripts
         Attack = 2,
     }
 
+    public enum MonsterType
+    {
+        Add = 0,
+        Multiply = 1,
+        Divide = 2,
+    }
+    
     public class MonsterController : MonoBehaviour
     {
 
-
-        [SerializeField] private MonsterState _state;
+        public MonsterType type;
+        public MonsterState state;
+        public int points = 0;
         [SerializeField] private float _findNewPositionEvery = 2f;
         [SerializeField] private float _maxMoveDistance = 10f;
 
         private float _wanderTimer = 0;
         private NavMeshAgent _agent;
-
+        
         // Start is called before the first frame update
-        void Awake()
+        void Start()
         {
             _agent = GetComponent<NavMeshAgent>();
-
+            _agent.enabled = false;
+            
             //add random factor when spawned as to visually stagger finding move positions
             var halfNewPositionEvery = _findNewPositionEvery;
             _findNewPositionEvery += Random.Range(-halfNewPositionEvery, halfNewPositionEvery);
@@ -36,9 +45,10 @@ namespace TutorialAssets.Scripts
         // Update is called once per frame
         void Update()
         {
-            switch (_state)
+            switch (state)
             {
                 case MonsterState.Wander:
+                    _agent.enabled = true;
                     _wanderTimer += Time.deltaTime;
 
                     if (_wanderTimer > _findNewPositionEvery)
@@ -46,13 +56,15 @@ namespace TutorialAssets.Scripts
                         FindNewWanderPosition();
                         _wanderTimer = 0;
                     }
-
+                    
                     break;
                 case MonsterState.Queue:
-
+                    _agent.enabled = false;
+                    
                     break;
                 case MonsterState.Attack:
-
+                    _agent.enabled = false;
+                    
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
@@ -63,19 +75,6 @@ namespace TutorialAssets.Scripts
         {
             var newPos = new Vector3(Random.Range(-_maxMoveDistance, _maxMoveDistance), 0, Random.Range(-_maxMoveDistance, _maxMoveDistance));
             _agent.SetDestination(transform.position + newPos);
-        }
-
-        public void ChangeState(MonsterState state)
-        {
-            _state = state;
-            if (state == MonsterState.Wander)
-            {
-                _agent.enabled = true;
-            }
-            else
-            {
-                _agent.enabled = false;
-            }
         }
     }
 }
