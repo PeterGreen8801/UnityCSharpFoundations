fa0ddb1bb08a9772d76eeff450cae4716b77e0b2
Peter <petergreen880@yahoo.com>
2024-01-22 19:23:44 -0500

    Added Player Health

---
+++ MonsterMathIsland/Assets/Scripts/Health.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _hp;
+    [SerializeField] private Transform _healthBarUI;
+
+    public int maxHp = 150;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateHealthBarUI();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _hp -= damage;
+
+        if (_hp <= 0)
+        {
+            _hp = 0;
+        }
+        UpdateHealthBarUI();
+    }
+
+    private void UpdateHealthBarUI()
+    {
+        if (!_healthBarUI)
+        {
+            return;
+        }
+
+        _healthBarUI.GetChild(1).GetComponent<Image>().fillAmount = (float)_hp / maxHp;
+        _healthBarUI.GetChild(2).GetComponent<TMP_Text>().text = $"{_hp} / {maxHp}";
+    }
+
+
+}
